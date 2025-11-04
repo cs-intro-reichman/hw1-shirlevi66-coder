f1de7ffefab68c1d6a8a3165394d93b489909583
shir levi <shirlevi66@gmail.com>
2025-11-04 13:00:36 +0200

    mesage

--- Demo0.java
+++ Demo0.java
@@ -1,9 +1,16 @@
 // Prints the numbers 0 to 5.
 // (Same program as shown in lecture 1-1).
-public class PrintTo5 {
+public class Demo0{
 	public static void main(String[] args) {
 		// Declares an int variable, and sets it to 1
         int i = 0;
+        for(i=0; i<=5; i++) {
+        	System.out.println(i);
+        if (i==5) {
+            System.out.println("done");
+        }
+        }
         // Complete the program's code here:
+        
+        }
     }
- }
